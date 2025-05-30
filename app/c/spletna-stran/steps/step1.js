081095b01bb9d4c0d4695ddeb242ee862fd4d9bf
Mohsin Zafar <heykhattak@gmail.com>
2025-05-30 12:31:10 +0500

    changes in character limit

--- app/c/spletna-stran/steps/step1.js
+++ app/c/spletna-stran/steps/step1.js
@@ -20,26 +20,26 @@ export default function Step1({ handleStepChange }) {
     setSelectedImage(imageFile);
   };
 
-const handleSave = async (e) => {
-  e.preventDefault();
-  const formData = new FormData();
+  const handleSave = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
 
-  formData.append("companyNameOrAddress", companyNameOrAddress);
-  formData.append("phone", phone);
-  formData.append("title", title);
-  formData.append("description", description);
+    formData.append("companyNameOrAddress", companyNameOrAddress);
+    formData.append("phone", phone);
+    formData.append("title", title);
+    formData.append("description", description);
 
-  if (selectedImage) {
-    formData.append("picture", selectedImage);
-  }
+    if (selectedImage) {
+      formData.append("picture", selectedImage);
+    }
 
-  try {
-    const result = await submitStep1Data(formData);
-    console.log("Saved successfully:", result);
-  } catch (err) {
-    console.error("Failed to save step 1:", err);
-  }
-};
+    try {
+      const result = await submitStep1Data(formData);
+      console.log("Saved successfully:", result);
+    } catch (err) {
+      console.error("Failed to save step 1:", err);
+    }
+  };
 
   return (
     <>
@@ -47,108 +47,196 @@ const handleSave = async (e) => {
         Blue Daisy Florist, London
       </div>
       <form method="POST" enctype="multipart/form-data">
-      <div className="min-h-full flex flex-col justify-between gap-[16px] relative">
-        <div className="space-y-[43px]">
-          <div className="flex justify-between items-center">
-            <div className="inline-flex gap-[17px] items-center">
-              <div className="w-[48px] h-[48px] rounded-full bg-[#D4DCF0] border border-[#0A85C2] flex items-center justify-center text-[20px] leading-[100%] text-[#0A85C2]">
-                1
+        <div className="min-h-full flex flex-col justify-between gap-[16px] relative">
+          <div className="space-y-[43px]">
+            <div className="flex justify-between items-center">
+              <div className="inline-flex gap-[17px] items-center">
+                <div className="w-[48px] h-[48px] rounded-full bg-[#D4DCF0] border border-[#0A85C2] flex items-center justify-center text-[20px] leading-[100%] text-[#0A85C2]">
+                  1
+                </div>
+                <div className="space-y-0">
+                  <div className="text-[14px] text-[#6D778E] leading-[24px] font-normal">
+                    KORAK 1
+                  </div>
+                  <div className="text-[20px] text-[#1E2125] leading-[100%] font-semibold">
+                    Zgornji blok
+                  </div>
+                </div>
               </div>
-              <div className="space-y-0">
-                <div className="text-[14px] text-[#6D778E] leading-[24px] font-normal">KORAK 1</div>
-                <div className="text-[20px] text-[#1E2125] leading-[100%] font-semibold">Zgornji blok</div>
+              <div className="inline-flex gap-[8px]">
+                <span className="text-[14px] text-[#3C3E41] leading-[24px]">
+                  Predogled strani
+                </span>
+                <Image
+                  src="/external_open.png"
+                  alt="Predogled strani"
+                  width={20}
+                  height={20}
+                  className="shrink-0 w-[20px] h-[20px]"
+                />
               </div>
             </div>
-            <div className="inline-flex gap-[8px]">
-              <span className="text-[14px] text-[#3C3E41] leading-[24px]">
-                Predogled strani
-              </span>
-              <Image src="/external_open.png" alt="Predogled strani" width={20} height={20} className="shrink-0 w-[20px] h-[20px]" />
-            </div>
-          </div>
-          <div className="space-y-[8px]">
-            <OpenableBlock isDefaultOpen={openedBlock === 1} title="Zgornja vrstica" index={1} havingSpacing={true} openBlock={openedBlock === 1} handleOpenBlock={() => setOpenedBlock(1)}>
-              <div className="space-y-[8px]">
-                <span className="text-[16px] text-[#3C3E41] font-normal leading-[24px]">Ime cvetličarne oz podjetja in kraj</span>
-                <input type="text" value={companyNameOrAddress} onChange={(e) => setCompanyNameOrAddress(e.target.value)} className="w-full border border-[#6D778E] bg-[#FFFFFF] outline-none rounded-[8px] py-[12px] px-[20px] text-[16px] text-[#3C3E41] placeholder:text-[#ACAAAA] leading-[24px]" placeholder="Cvetličarna Suniflower, Milano" />
-              </div>
-              <div className="space-y-[8px]">
-                <div className="flex items-center gap-[8px]">
-                  <Image src="/phone_icon.png" alt="Spletna stran" width={24} height={24} />
+            <div className="space-y-[8px]">
+              <OpenableBlock
+                isDefaultOpen={openedBlock === 1}
+                title="Zgornja vrstica"
+                index={1}
+                havingSpacing={true}
+                openBlock={openedBlock === 1}
+                handleOpenBlock={() => setOpenedBlock(1)}
+              >
+                <div className="space-y-[8px]">
                   <span className="text-[16px] text-[#3C3E41] font-normal leading-[24px]">
-                    Telefonska številka
+                    Ime cvetličarne oz podjetja in kraj
+                  </span>
+                  <input
+                    type="text"
+                    value={companyNameOrAddress}
+                    onChange={(e) => setCompanyNameOrAddress(e.target.value)}
+                    className="w-full border border-[#6D778E] bg-[#FFFFFF] outline-none rounded-[8px] py-[12px] px-[20px] text-[16px] text-[#3C3E41] placeholder:text-[#ACAAAA] leading-[24px]"
+                    placeholder="Cvetličarna Suniflower, Milano"
+                  />
+                </div>
+                <div className="space-y-[8px]">
+                  <div className="flex items-center gap-[8px]">
+                    <Image
+                      src="/phone_icon.png"
+                      alt="Spletna stran"
+                      width={24}
+                      height={24}
+                    />
+                    <span className="text-[16px] text-[#3C3E41] font-normal leading-[24px]">
+                      Telefonska številka
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    className="w-full border border-[#6D778E] bg-[#FFFFFF] outline-none rounded-[8px] py-[12px] px-[20px] text-[16px] text-[#3C3E41] placeholder:text-[#ACAAAA] leading-[24px]"
+                    placeholder="055-083-916"
+                  />
+                </div>
+              </OpenableBlock>
+              <OpenableBlock
+                isDefaultOpen={openedBlock === 2}
+                title="Velika zgornja slika"
+                index={2}
+                openBlock={openedBlock === 2}
+                handleOpenBlock={() => setOpenedBlock(2)}
+              >
+                <div className="space-y-[8px]">
+                  <span className="text-[14px] text-[#3C3E41] font-normal leading-[24px]">
+                    Začasno lahko izbereš eno od teh slik in svojo prilepiš
+                    naknadno.
                   </span>
                 </div>
-                <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} className="w-full border border-[#6D778E] bg-[#FFFFFF] outline-none rounded-[8px] py-[12px] px-[20px] text-[16px] text-[#3C3E41] placeholder:text-[#ACAAAA] leading-[24px]" placeholder="055-083-916" />
-              </div>
-            </OpenableBlock>
-            <OpenableBlock isDefaultOpen={openedBlock === 2} title="Velika zgornja slika" index={2} openBlock={openedBlock === 2} handleOpenBlock={() => setOpenedBlock(2)}>
-              <div className="space-y-[8px]">
-                <span className="text-[14px] text-[#3C3E41] font-normal leading-[24px]">Začasno lahko izbereš eno od teh slik in svojo prilepiš naknadno.</span>
-              </div>
-              <BackgroundSelector />
-              <div className="space-y-[8px]">
-                <span className="text-[16px] text-[#3C3E41] font-normal leading-[24px]">Ime cvetličarne oz podjetja in kraj</span>
-                <ImageSelector onImageSelect={handleImageSelect}/>
-              </div>
-              <div className="text-center text-[14px] leading-[24px] text-[#3C3E41] pt-[5px] pb-[10px]">
-              --------------
-              </div>
-              <div className="flex items-center gap-[12px]">
-                <span className="text-[16px] leading-[24px] text-[#ACAAAA]">Steklen okvirček</span>
-                <Image src="/question_mark.png" width={24} height={24} alt="Spletna stran" />
-              </div>
-              <div className="flex items-center justify-between gap-[12px] py-[2px]">
-                <span className="text-[16px] text-[#3C3E41] font-normal leading-[24px]">Prikaži steklen okvir</span>
-                <Switch />
-                <span className="text-[16px] text-[#3C3E41] font-normal leading-[24px]">Izbriši steklen okvir</span>
-              </div>
-              <div className="space-y-[8px]">
-                <span className="text-[16px] text-[#3C3E41] font-normal leading-[24px]">
-                  Naslov v okvirčku
-                </span>
-                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className="w-full border border-[#6D778E] bg-[#FFFFFF] outline-none rounded-[8px] py-[12px] px-[20px] text-[16px] text-[#3C3E41] placeholder:text-[#ACAAAA] leading-[24px]" placeholder="Cvetličarna z dolgoletno tradicijo" />
-              </div>
-              <div className="space-y-[8px]">
-                <span className="text-[16px] text-[#3C3E41] font-normal leading-[24px]">
-                Tekst
-                </span>
-                <textarea type="text" value={description} onChange={(e) => setDescription(e.target.value)} className="w-full border border-[#6D778E] bg-[#FFFFFF] outline-none rounded-[8px] py-[12px] px-[20px] min-h-[108px] text-[14px] text-[#3C3E41] placeholder:text-[#ACAAAA] leading-[24px]" placeholder="V Cvetličarni Suniflower, na levem bregu Ljubljanice, že 22 let širimo ljubezen do cvetličarske umetnosti. Nudimo vam rezano cvetje, lončnice, " />
-              </div>
-            </OpenableBlock>
+                <BackgroundSelector />
+                <div className="space-y-[8px]">
+                  <span className="text-[16px] text-[#3C3E41] font-normal leading-[24px]">
+                    Ime cvetličarne oz podjetja in kraj
+                  </span>
+                  <ImageSelector onImageSelect={handleImageSelect} />
+                </div>
+                <div className="text-center text-[14px] leading-[24px] text-[#3C3E41] pt-[5px] pb-[10px]">
+                  --------------
+                </div>
+                <div className="flex items-center gap-[12px]">
+                  <span className="text-[16px] leading-[24px] text-[#ACAAAA]">
+                    Steklen okvirček
+                  </span>
+                  <Image
+                    src="/question_mark.png"
+                    width={24}
+                    height={24}
+                    alt="Spletna stran"
+                  />
+                </div>
+                <div className="flex items-center justify-between gap-[12px] py-[2px]">
+                  <span className="text-[16px] text-[#3C3E41] font-normal leading-[24px]">
+                    Prikaži steklen okvir
+                  </span>
+                  <Switch />
+                  <span className="text-[16px] text-[#3C3E41] font-normal leading-[24px]">
+                    Izbriši steklen okvir
+                  </span>
+                </div>
+                <div className="space-y-[8px]">
+                  <span className="text-[16px] text-[#3C3E41] font-normal leading-[24px]">
+                    Naslov v okvirčku
+                  </span>
+                  <input
+                    type="text"
+                    maxLength={25}
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    className="w-full border border-[#6D778E] bg-[#FFFFFF] outline-none rounded-[8px] py-[12px] px-[20px] text-[16px] text-[#3C3E41] placeholder:text-[#ACAAAA] leading-[24px]"
+                    placeholder="Cvetličarna z dolgoletno tradicijo"
+                  />
+                </div>
+                <div className="space-y-[8px]">
+                  <span className="text-[16px] text-[#3C3E41] font-normal leading-[24px]">
+                    Tekst
+                  </span>
+                  <textarea
+                    type="text"
+                    value={description}
+                    maxLength={220}
+                    onChange={(e) => setDescription(e.target.value)}
+                    className="w-full border border-[#6D778E] bg-[#FFFFFF] outline-none rounded-[8px] py-[12px] px-[20px] min-h-[108px] text-[14px] text-[#3C3E41] placeholder:text-[#ACAAAA] leading-[24px]"
+                    placeholder="V Cvetličarni Suniflower, na levem bregu Ljubljanice, že 22 let širimo ljubezen do cvetličarske umetnosti. Nudimo vam rezano cvetje, lončnice, "
+                  />
+                </div>
+              </OpenableBlock>
+            </div>
           </div>
-        </div>
-        <div className="space-y-[48px]">
-          {openedBlock === 1 && <div className="space-y-[8px]">
-            <p className="text-[14px] leading-[20px] text-[#6D778E]">
-            Tako enostavno je!  
-            </p>
-            <p className="text-[14px] leading-[20px] text-[#6D778E]">
-              Nadaljujete postopek po korakih in vmes občasno tudi shranjujte in na zadnjem koraku pritisnite gumb OBJAVI. 
-            </p>
-            <p className="text-[14px] leading-[20px] text-[#6D778E]">
-            Vašo stran bomo izdelali in objavili najkasneje v 48 urah.  
-            </p>
-          </div>}
-          <div className="flex items-center gap-[8px] justify-between w-full">
-            <button onClick={handleSave} className="bg-[#3DA34D] text-[#FFFFFF] font-normal leading-[24px] text-[16px] py-[12px] px-[25px] rounded-[8px]">
-            Shrani
-            </button>
-            <div className="flex items-center gap-[8px]">
-              <button className="bg-gradient-to-r from-[#E3E8EC] to-[#FFFFFF] text-[#1E2125] font-normal leading-[24px] text-[16px] py-[12px] px-[25px] rounded-[8px] shadow-[5px_5px_10px_0px_rgba(194,194,194,0.5)]" onClick={() => handleStepChange(1)}>
-                Nazaj
+          <div className="space-y-[48px]">
+            {openedBlock === 1 && (
+              <div className="space-y-[8px]">
+                <p className="text-[14px] leading-[20px] text-[#6D778E]">
+                  Tako enostavno je!
+                </p>
+                <p className="text-[14px] leading-[20px] text-[#6D778E]">
+                  Nadaljujete postopek po korakih in vmes občasno tudi
+                  shranjujte in na zadnjem koraku pritisnite gumb OBJAVI.
+                </p>
+                <p className="text-[14px] leading-[20px] text-[#6D778E]">
+                  Vašo stran bomo izdelali in objavili najkasneje v 48 urah.
+                </p>
+              </div>
+            )}
+            <div className="flex items-center gap-[8px] justify-between w-full">
+              <button
+                onClick={handleSave}
+                className="bg-[#3DA34D] text-[#FFFFFF] font-normal leading-[24px] text-[16px] py-[12px] px-[25px] rounded-[8px]"
+              >
+                Shrani
               </button>
-              <button className="bg-gradient-to-r from-[#E3E8EC] to-[#FFFFFF] text-[#1E2125] font-normal leading-[24px] text-[16px] py-[12px] px-[25px] rounded-[8px] shadow-[5px_5px_10px_0px_rgba(194,194,194,0.5)]" onClick={() => handleStepChange(2)}>
-                Naslednji korak
-              </button>
+              <div className="flex items-center gap-[8px]">
+                <button
+                  className="bg-gradient-to-r from-[#E3E8EC] to-[#FFFFFF] text-[#1E2125] font-normal leading-[24px] text-[16px] py-[12px] px-[25px] rounded-[8px] shadow-[5px_5px_10px_0px_rgba(194,194,194,0.5)]"
+                  onClick={() => handleStepChange(1)}
+                >
+                  Nazaj
+                </button>
+                <button
+                  className="bg-gradient-to-r from-[#E3E8EC] to-[#FFFFFF] text-[#1E2125] font-normal leading-[24px] text-[16px] py-[12px] px-[25px] rounded-[8px] shadow-[5px_5px_10px_0px_rgba(194,194,194,0.5)]"
+                  onClick={() => handleStepChange(2)}
+                >
+                  Naslednji korak
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </form>
       <div className="w-full">
-        <img src="/florist/1.png" alt="Spletna stran" className="w-full h-full object-contain" />
+        <img
+          src="/florist/1.png"
+          alt="Spletna stran"
+          className="w-full h-full object-contain"
+        />
       </div>
     </>
-  )
+  );
 }
--- app/c/spletna-stran/steps/step5.js
+++ app/c/spletna-stran/steps/step5.js
@@ -12,12 +12,12 @@ export default function Step5({ handleStepChange }) {
       id: 1,
       title: "Slike vaše ponudbe",
       isDefaultOpen: true,
-    }
+    },
   ]);
 
   const addSliderBlock = () => {
     setSliderBlocks([...sliderBlocks, { id: sliderBlocks.length + 1 }]);
-  }
+  };
 
   return (
     <>
@@ -32,28 +32,55 @@ export default function Step5({ handleStepChange }) {
                 5
               </div>
               <div className="space-y-0">
-                <div className="text-[14px] text-[#6D778E] leading-[24px] font-normal">KORAK 5</div>
-                <div className="text-[20px] text-[#1E2125] leading-[100%] font-semibold">Premični blok - slajder</div>
+                <div className="text-[14px] text-[#6D778E] leading-[24px] font-normal">
+                  KORAK 5
+                </div>
+                <div className="text-[20px] text-[#1E2125] leading-[100%] font-semibold">
+                  Premični blok - slajder
+                </div>
               </div>
             </div>
             <div className="inline-flex gap-[8px]">
               <span className="text-[14px] text-[#3C3E41] leading-[24px]">
                 Predogled strani
               </span>
-              <Image src="/external_open.png" alt="Predogled strani" width={20} height={20} className="shrink-0 w-[20px] h-[20px]" />
+              <Image
+                src="/external_open.png"
+                alt="Predogled strani"
+                width={20}
+                height={20}
+                className="shrink-0 w-[20px] h-[20px]"
+              />
             </div>
           </div>
           <div className="space-y-[8px]">
             <div className="space-y-[8px] pb-[38px] text-[14px] text-[#6D778E] leading-[20px]">
-              Povsem vaš blok, kjer lahko dodate veliko teksta. Slika bo avtomatsko optimizirana, zato bo (skoraj) vsaka izgledala lepo. Dodate lahko do tri take bloke, ki se samodejno izmenjavajo. Vsebino v tem bloku lahko neprestano prilagajate priložnostim ali svojim promocijam.  
+              Povsem vaš blok, kjer lahko dodate veliko teksta. Slika bo
+              avtomatsko optimizirana, zato bo (skoraj) vsaka izgledala lepo.
+              Dodate lahko do tri take bloke, ki se samodejno izmenjavajo.
+              Vsebino v tem bloku lahko neprestano prilagajate priložnostim ali
+              svojim promocijam.
             </div>
             {sliderBlocks.map((block) => (
-              <SliderBlock key={block.id} index={block.id} title={`Slajd ${block.id}`} />
+              <SliderBlock
+                key={block.id}
+                index={block.id}
+                title={`Slajd ${block.id}`}
+              />
             ))}
             <div className="flex items-center justify-end pt-[8px] pb-[16px]">
-              <div className="inline-flex items-center gap-[8px] cursor-pointer" onClick={addSliderBlock}>
-                <img src="/florist_plus.png" alt="Dodaj sliko" className="w-[16px] h-[16px]" />
-                <span className="text-[14px] text-[#3C3E41] font-normal leading-[100%]">DODAJ ŠE EN SLAJD</span>
+              <div
+                className="inline-flex items-center gap-[8px] cursor-pointer"
+                onClick={addSliderBlock}
+              >
+                <img
+                  src="/florist_plus.png"
+                  alt="Dodaj sliko"
+                  className="w-[16px] h-[16px]"
+                />
+                <span className="text-[14px] text-[#3C3E41] font-normal leading-[100%]">
+                  DODAJ ŠE EN SLAJD
+                </span>
               </div>
             </div>
           </div>
@@ -61,13 +88,19 @@ export default function Step5({ handleStepChange }) {
         <div className="space-y-[8px]">
           <div className="flex items-center gap-[8px] justify-between w-full">
             <button className="bg-[#3DA34D] text-[#FFFFFF] font-normal leading-[24px] text-[16px] py-[12px] px-[25px] rounded-[8px]">
-            Shrani
+              Shrani
             </button>
             <div className="flex items-center gap-[8px]">
-              <button className="bg-gradient-to-r from-[#E3E8EC] to-[#FFFFFF] text-[#1E2125] font-normal leading-[24px] text-[16px] py-[12px] px-[25px] rounded-[8px] shadow-[5px_5px_10px_0px_rgba(194,194,194,0.5)]" onClick={() => handleStepChange(4)}>
+              <button
+                className="bg-gradient-to-r from-[#E3E8EC] to-[#FFFFFF] text-[#1E2125] font-normal leading-[24px] text-[16px] py-[12px] px-[25px] rounded-[8px] shadow-[5px_5px_10px_0px_rgba(194,194,194,0.5)]"
+                onClick={() => handleStepChange(4)}
+              >
                 Nazaj
               </button>
-              <button className="bg-gradient-to-r from-[#E3E8EC] to-[#FFFFFF] text-[#1E2125] font-normal leading-[24px] text-[16px] py-[12px] px-[25px] rounded-[8px] shadow-[5px_5px_10px_0px_rgba(194,194,194,0.5)]" onClick={() => handleStepChange(6)}>
+              <button
+                className="bg-gradient-to-r from-[#E3E8EC] to-[#FFFFFF] text-[#1E2125] font-normal leading-[24px] text-[16px] py-[12px] px-[25px] rounded-[8px] shadow-[5px_5px_10px_0px_rgba(194,194,194,0.5)]"
+                onClick={() => handleStepChange(6)}
+              >
                 Naslednji korak
               </button>
             </div>
@@ -75,10 +108,14 @@ export default function Step5({ handleStepChange }) {
         </div>
       </div>
       <div className="w-full">
-        <img src="/florist/5.png" alt="Spletna stran" className="w-full h-full object-contain" />
+        <img
+          src="/florist/5.png"
+          alt="Spletna stran"
+          className="w-full h-full object-contain"
+        />
       </div>
     </>
-  )
+  );
 }
 
 function SliderBlock({ index, title }) {
@@ -88,23 +125,32 @@ function SliderBlock({ index, title }) {
       <div className="space-y-[16px]">
         <div className="space-y-[8px]">
           <div className="text-[16px] text-[#3C3E41] font-normal leading-[24px]">
-          Slika
+            Slika
           </div>
           <ImageSelector />
         </div>
         <div className="space-y-[8px]">
           <label className="text-[16px] text-[#3C3E41] font-normal leading-[24px]">
-          Naslov
+            Naslov
           </label>
-          <input type="text" className="w-full border border-[#6D778E] bg-[#FFFFFF] outline-none rounded-[8px] py-[12px] px-[20px] text-[16px] text-[#3C3E41] placeholder:text-[#ACAAAA] leading-[24px]" placeholder="Posebna ponudba v avgustu" />
+          <input
+            type="text"
+            className="w-full border border-[#6D778E] bg-[#FFFFFF] outline-none rounded-[8px] py-[12px] px-[20px] text-[16px] text-[#3C3E41] placeholder:text-[#ACAAAA] leading-[24px]"
+            placeholder="Posebna ponudba v avgustu"
+          />
         </div>
         <div className="space-y-[8px]">
           <label className="text-[16px] text-[#3C3E41] font-normal leading-[24px]">
             Tekst
           </label>
-          <textarea type="text" className="w-full border border-[#6D778E] bg-[#FFFFFF] outline-none rounded-[8px] py-[12px] px-[20px] text-[14px] text-[#3C3E41] placeholder:text-[#ACAAAA] leading-[24px] min-h-[108px]" placeholder="Visok blok, kjer lahko dodamo karkoli, kar bi lahko pritegnilo vaše stranke ali poudarite posebne promocije ali ponudbo med prazniki, ipd. " />
+          <textarea
+            maxLength={220}
+            type="text"
+            className="w-full border border-[#6D778E] bg-[#FFFFFF] outline-none rounded-[8px] py-[12px] px-[20px] text-[14px] text-[#3C3E41] placeholder:text-[#ACAAAA] leading-[24px] min-h-[108px]"
+            placeholder="Visok blok, kjer lahko dodamo karkoli, kar bi lahko pritegnilo vaše stranke ali poudarite posebne promocije ali ponudbo med prazniki, ipd. "
+          />
         </div>
       </div>
     </OpenableBlock>
-  )
+  );
 }

1d5840e7120aa087e2cb341256a674875d65bbd3
ehtishamabbas <ehtishamabbas918@gmail.com>
2025-06-25 22:12:19 +0500

    updated celander section

--- app/components/appcomponents/FloristsComp.js
+++ app/components/appcomponents/FloristsComp.js
@@ -44,21 +44,25 @@ const FloristsComp = () => {
   return (
     <div className="flex w-full flex-col border-b-[1px] border-[#C7C7C7]">
       <div className="flex w-full ">
-        <div className="relative flex flex-row min-w-full dekstop:h-[300px] tablet:h-[361px] mobile:h-[236px]">
+        <div className="relative flex flex-row min-w-full dekstop:h-[300px] tablet:h-[230px] mobile:h-[236px]">
+
           <img
             src="/flower38.png"
             alt="Flower_img"
             className="h-[100%] w-[100%] desktop:w-[50%] hidden tablet:flex desktop:flex  object-cover"
           />
+
           <img
             src="/flower38.png"
             alt="Slika"
             className="h-full w-full tablet:hidden desktop:hidden object-cover"
           />
+
+
           <div
             className="
                             flex mobile:absolute tablet:absolute  
-                            tablet:justify-end tablet:items-center desktop:items-center desktop:justify-center desktop:bg-[#FFE5B4]                             
+                            tablet:justify-start tablet:pl-11 tablet:items-center desktop:items-center desktop:justify-center desktop:bg-[#FFE5B4]                             
                             h-full w-full   desktop:w-[50%]   "
           >
             <div
@@ -70,11 +74,11 @@ const FloristsComp = () => {
               <div
                 className="flex flex-col items-center justify-between
                                 h-full w-full
-                                mobile:w-[293px] mobile:h-[110px]
+                                mobile:w-[330px] mobile:h-[110px]
                                 desktop:w-[476px] desktop:h-[124px]
-                                tablet:h-[167px] tablet:w-[391px] tablet:py-[22px]
-                                tablet:rounded-2xl tablet:border-2
-                                tablet:border-[#FFFFFF] tablet:shadow-custom-light-dark-banner tablet:bg-gradient-to-r tablet:from-[#FFFFFF] tablet:to-[#FFFFFF50] tablet:backdrop-blur-sm
+                                tablet:h-[167px] tablet:w-[391px] tablet:py-[22px] mobile:py-[22px]
+                                tablet:rounded-2xl mobile:border-2 tablet:border-2
+                                tablet:border-[#FFFFFF] mobile:border-[#FFFFFF] tablet:shadow-custom-light-dark-banner mobile:shadow-custom-light-dark-banner tablet:bg-gradient-to-r mobile:bg-gradient-to-r tablet:from-[#FFFFFF] mobile:from-[#FFFFFF] tablet:to-[#FFFFFF50] mobile:to-[#FFFFFF50] tablet:backdrop-blur-sm mobile:backdrop-blur-sm
                                 "
               >
                 <div className="flex mobile:w-full mobile:justify-end items-center h-[47px] mobile:h-[33px] ">

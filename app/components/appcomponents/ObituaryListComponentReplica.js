ddb6d113213e8c3f099cc206027d1724cb6c1579
ehtishamabbas <ehtishamabbas918@gmail.com>
2025-06-25 21:38:21 +0500

    updated celander section

--- app/components/appcomponents/ObituaryListComponentReplica.js
+++ app/components/appcomponents/ObituaryListComponentReplica.js
@@ -1,7 +1,25 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Dropdown from "@/app/components/appcomponents/Dropdown";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import regionsAndCities from "@/utils/regionAndCities";
+import { toast } from "react-hot-toast";
 
 const CarouselCalendar = () => {
+  const languages = [
+    "Ljubljana",
+    "Maribor",
+    "Celje",
+    "Kranj",
+    "Koper",
+    "Novo Mesto",
+    "Domžale",
+    "Velenje",
+    "Nova Gorica",
+    "Koroška",
+    "Zasavje",
+  ];
+
   // --- 5-Day Carousel Calendar ---
   // Responsive card sizes
   const [screen, setScreen] = useState("desktop");
@@ -165,11 +183,100 @@ const CarouselCalendar = () => {
     return abbr.charAt(0).toUpperCase() + abbr.slice(1);
   };
 
+  const allRegionsOption = {
+    place: "- Pokaži vse regije - ",
+    id: "allRegions",
+  };
+  const [selectedRegion, setSelectedRegion] = useState(null);
+  const [selectedCity, setSelectedCity] = useState(null);
+  const allCitiesOption = { place: " - Pokaži vse občine - ", id: "allCities" };
+
+  const regionOptions = [
+    allRegionsOption,
+    ...Object.keys(regionsAndCities).map((region) => ({
+      place: region,
+      id: region,
+    })),
+  ];
+
+  const handleRegionSelect = (item) => {
+    if (item.id === "allRegions") {
+      setSelectedRegion(null);
+      setSelectedCity(null);
+      return;
+    }
+    setSelectedRegion(item.place);
+    setSelectedCity(null);
+  };
+
+  const cityOptions =
+    selectedRegion && selectedRegion !== "allRegions"
+      ? [
+          allCitiesOption,
+          ...regionsAndCities[selectedRegion].map((city) => ({
+            place: city,
+            id: city,
+          })),
+        ]
+      : [
+          allCitiesOption,
+          ...Object.values(regionsAndCities)
+            .flat()
+            .map((city) => ({
+              place: city,
+              id: city,
+            }))
+            .sort((a, b) => a.place.localeCompare(b.place, "sl")),
+        ];
+
+  const handleCitySelect = (item) => {
+    if (item.id === "allCities") {
+      setSelectedCity(null);
+      return;
+    }
+    setSelectedCity(item.place);
+    setSelectedRegion(null);
+  };
+
+  useEffect(() => {
+    fetchObituary();
+  }, []);
+
+  const fetchObituary = async () => {
+    try {
+      const queryParams = {};
+
+      if (selectedCity) queryParams.city = selectedCity;
+
+      if (selectedRegion) queryParams.region = selectedRegion;
+      const response = await obituaryService.getObituary(queryParams);
+
+      if (response.error) {
+        toast.error(
+          response.error || "Something went wrong. Please try again!"
+        );
+        return;
+      }
+
+      const sortedObituaries = response.obituaries.sort(
+        (a, b) =>
+          new Date(b.deathDate).getTime() - new Date(a.deathDate).getTime()
+      );
+
+      setObituaries(sortedObituaries);
+    } catch (err) {
+      console.error("Error fetching obituary:", err);
+      toast.error(err.message || "Failed to fetch obituary.");
+    }
+  };
+
   return (
     <>
       <div
         className={`w-full flex flex-col items-center bg-white border-b-[1px] border-[#C7C7C7] ${
-          screen === "mobile" ? "py-[50px]" : "py-[100px]"
+          screen === "mobile" || screen === "tablet"
+            ? "py-[50px]"
+            : "py-[100px]"
         }`}
       >
         {/* Calendar 5-Day Carousel */}
@@ -629,7 +736,112 @@ const CarouselCalendar = () => {
             </button>
           </div>
         </div>
-        <div className="table-container mx-8 mt-[40px]">
+
+        <div className=" flex flex-col items-center w-full tablet:w-full mobile:w-full">
+          {/* Main container for inputs main container */}
+          <div className="w-full tablet:w-full mobile:w-full flex flex-col items-center">
+            {/* Inputs main Container */}
+            <div
+              className="w-[777px] tablet:w-[600px] h-[48px] tablet:h-[112px] tablet:columns-2 mobile:w-[296px] mobile:h-[240px] mobile:flex-wrap 
+            flex tablet:flex-wrap flex-row gap-4 mt-[69.07px] tablet:mt-[40px] mobile:mt-[40px] mobile:mb-[42px] tablet:mb-[53px] mb-[23.93px]"
+            >
+              {/* Input field for  Išči po imenu / priimku*/}
+              <div className="flex w-[227px] tablet:w-[292px] h-[48px] mobile:w-[296px] justify-center items-center">
+                <input
+                  type="text"
+                  placeholder="Išči po imenu / priimku"
+                  className="bg-white border-[#7C7C7C] placeholder-[#7C7C7C] text-[16px] font-[400] leading-[24px] border rounded-lg shadow-sm flex flex-1 items-center justify-between h-full px-4 text-[#7C7C7C] focus:outline-none"
+                />
+              </div>
+              {/* Dropdown for Išči po regiji*/}
+              <Dropdown
+                label={"Išči po regiji"}
+                isFromNotification={false}
+                isFromFlower={false}
+                data={regionOptions}
+                selectedValue={selectedRegion}
+                onSelect={handleRegionSelect}
+              />
+              {/* Dropdown for Išči po kraju*/}
+              <Dropdown
+                data={cityOptions}
+                label={"Išči po kraju"}
+                isFromNotification={false}
+                isFromFlower={false}
+                selectedValue={selectedCity}
+                onSelect={handleCitySelect}
+              />
+              {/* Search container and magnifying glass image */}
+              <div
+                onClick={() => fetchObituary()}
+                className="hidden desktop:flex justify-center items-center w-12 h-full desktop:aspect-square rounded-lg bg-[#414141]"
+              >
+                <MagnifyingGlassIcon className="w-5 h-5 text-white hidden desktop:block" />
+              </div>
+              <div className="tablet:w-[292px] mobile:w-[296px] h-[48px] mobile:text-[16px] mobile:text-[#F6F6F6] rounded-lg tablet:leading-6 tablet:text-[16px] tablet:text-[#F6F6F6] bg-[#414141] tablet:font-[400px] hidden tablet:flex mobile:flex justify-center items-center">
+                Prikaži
+              </div>
+            </div>
+          </div>
+
+          {/* Hitri izbor heading for desktop*/}
+          <div className="max-w-[1062px] w-full block mobile:hidden tablet:hidden text-[24px] font-[400px] leading-[28.13px] text-[#1E2125]">
+            <div>Hitri izbor</div>
+          </div>
+
+          {/* Hitri izbor heading and list for tablet and mobile*/}
+          <div className="tablet:w-[600px] mobile:w-[304px] hidden tablet:flex tablet:flex-row mobile:flex mobile:flex-col text-[24px] mobile:text-[28px] font-[400px] leading-[28.13px] text-[#1E2125]">
+            <div>
+              <div className="mr-[24px] mobile:mb-[17px] whitespace-nowrap">
+                Hitri izbor:
+              </div>
+            </div>
+            <div className="w-[504px] mobile:w-[307px]">
+              <div>
+                <ul className="flex flex-row flex-wrap list-none">
+                  {languages.map((language) => (
+                    <li key={language} className="flex items-center">
+                      <div
+                        className="mr-[14px] mobile:mr-[16px] mb-[12px] mobile:mb-[16px] border border-[#C3C6C8] rounded-sm text-[#3C3E41] 
+                        text-[14px] font-extrabold italic leading-[16.41px] px-[7.5px] py-[4px] 
+                        bg-gradient-to-br from-[#E3E8EC] to-[#FFFFFF] whitespace-nowrap"
+                      >
+                        {language}
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          {/* list horizontal container for desktop*/}
+          <div className="mt-[18px] block mobile:hidden tablet:hidden">
+            <ul className="flex flex-row list-none">
+              {languages.map((language, index) => (
+                <li key={language} className="flex items-center">
+                  {index == languages.length - 1 ? (
+                    <div
+                      className=" border border-[#C3C6C8] rounded-sm text-[#3C3E41] 
+                        text-[14px] font-extrabold italic leading-[16.41px] px-[7.5px] py-[4px] bg-gradient-to-br from-[#E3E8EC] to-[#FFFFFF]"
+                    >
+                      {language}
+                    </div>
+                  ) : (
+                    <div
+                      className="mr-[17px] border border-[#C3C6C8] rounded-sm text-[#3C3E41] 
+                        text-[14px] font-extrabold italic leading-[16.41px] px-[7.5px] py-[4px] bg-gradient-to-br from-[#E3E8EC] to-[#FFFFFF]"
+                    >
+                      {language}
+                    </div>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <div className="table-container mx-8 mt-[80px]">
           <table
             className={`${
               screen === "mobile"

b1a1aea14b7da85c7f0b41195c70f62d89d3735c
mm2439 <gamspob@yahoo.com>
2025-07-15 21:22:05 +0200

    Delete app directory
    Remove old /app directory (replacement incoming)

--- app/access-denied/page.js
+++
@@ -1,8 +0,0 @@
-export default function AccessDenied() {
-  return (
-    <div style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>Access Denied</h1>
-      <p>You do not have permission to view this page.</p>
-    </div>
-  );
-}

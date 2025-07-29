d4fda5cbe1758a4e9e7a2bc3453554d765380402
syed ali shah <ali.web009@gmail.com>
2025-07-29 15:25:07 +0500

    updated, resolve build issues

--- app/kontakt/page.js
+++ app/kontakt/page.js
@@ -7,13 +7,13 @@ import Layout from "../components/appcomponents/Layout";
 
 const ContactForm = () => {
     return (
-    <Layout from={"18"} forFooter={"memorypage"} currentPage="">
-        <div className="flex flex-1 flex-col bg-[#F5F7F9]">
-            {/* <HeaderRegistration/> */}
-            <ContactFormPage/>
-        </div>
-    </Layout>
-    
+        <Layout from={"18"} forFooter={"memorypage"} currentPage="">
+            <div className="flex flex-1  flex-col bg-[#F5F7F9]">
+                {/* <HeaderRegistration/> */}
+                <ContactFormPage />
+            </div>
+        </Layout>
+
     )
 }
 

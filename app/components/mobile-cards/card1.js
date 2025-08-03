f4f7264dc5fd27610fcb4a144e8e721e7fa5dd78
Muhammad Hasnaat Khalid <hasnaatkhalid911@gmail.com>
2025-08-03 22:44:21 +0500

    Revert "Merge pull request #55 from mm2439/hasnaat-auth-fix"
    This reverts commit fa394d03e04180a014461d63499fd30edd0edd1f, reversing
    changes made to 18d9ca2fb4c1bfc95df5f2ad5cec8d2856015e91.

--- app/components/mobile-cards/card1.js
+++ app/components/mobile-cards/card1.js
@@ -74,8 +74,7 @@ const Card1 = ({ data, cardRefs, index }) => {
               </h1>
               <p className="">{formatDate(data?.funeralTimestamp)}</p>
               <p className="text-[#fff] pb-10">
-                {/* {data?.Cemetry?.name} */}
-                Pokopališče
+                {data?.Cemetry?.funeralCemetery} v {data?.funeralLocation}
               </p>
             </div>
           </div>
--- app/components/mobile-cards/card2.js
+++ app/components/mobile-cards/card2.js
@@ -42,8 +42,7 @@ const Card2 = ({ data = {}, cardRefs, index }) => {
             <p className="text-[#78600B] text-center mx-auto mt-3 text-[14px] font-light">
               {formatDayAndTimeSlovenian(data?.funeralTimestamp).dayName} ob{" "}
               {formatDayAndTimeSlovenian(data?.funeralTimestamp).time} <br />{" "}
-              {/* {data?.Cemetry?.name} */}
-              Pokopališče
+              {data?.Cemetry?.funeralCemetery} v {data?.funeralLocation}
             </p>
           </div>
         </div>

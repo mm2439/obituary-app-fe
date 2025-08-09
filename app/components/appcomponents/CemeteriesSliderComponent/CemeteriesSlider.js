0d870dc0ead9c41ebd9df38b41cb9482105d3ac1
uzair888 <uzairsarwar10@gmail.com>
2025-08-10 00:00:47 +0500

    Fixed the issues metioned in make their page doc

--- app/components/appcomponents/CemeteriesSliderComponent/CemeteriesSlider.js
+++ app/components/appcomponents/CemeteriesSliderComponent/CemeteriesSlider.js
@@ -7,6 +7,8 @@ import API_BASE_URL from "@/config/apiConfig";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function ImageCarousel({ images }) {
+  const [dotStartIndex, setDotStartIndex] = React.useState(0);
+  const visibleDots = 6; // Number of dots to show at once
   const settings = {
     dots: true,
     infinite: true,
@@ -17,11 +19,73 @@ export default function ImageCarousel({ images }) {
     autoplay: false,
     autoplaySpeed: 3000,
     cssEase: "ease-in-out",
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (oldIndex, newIndex) => {
+      // Change the visible dots window
+      if (newIndex >= dotStartIndex + visibleDots) {
+        setDotStartIndex(newIndex - visibleDots + 1);
+      } else if (newIndex < dotStartIndex) {
+        setDotStartIndex(newIndex);
+      }
+    },
+    appendDots: (dots) => {
+      const totalDots = React.Children.count(dots);
+      const visible = dots.slice(dotStartIndex, dotStartIndex + visibleDots);
+
+      return (
+        <div style={{ bottom: "10px" }}>
+          <ul className="slick-dots">{visible}</ul>
+        </div>
+      );
+    },
+    customPaging: (i) => (
+      <button type="button">
+        {/* Empty button so slick styles it with ::before */}
+      </button>
+    ),
   };
 
-  console.log(images, "Images for carousel");
+  function NextArrow(props) {
+    const { onClick } = props;
+    return (
+      <div
+        onClick={onClick}
+        className="absolute top-[45%] right-3 -translate-y-1/2 z-10 cursor-pointer"
+        style={{ userSelect: "none" }}
+      >
+        <Image
+          src="/right_gray_icon.png"
+          alt="Slika"
+          width={16}
+          height={20}
+          className="cursor-pointer"
+        />
+      </div>
+    );
+  }
+
+  function PrevArrow(props) {
+    const { onClick } = props;
+    return (
+      <div
+        onClick={onClick}
+        className="absolute top-[45%] left-3 -translate-y-1/2 z-10 cursor-pointer"
+        style={{ userSelect: "none" }}
+      >
+        <Image
+          src="/left_gray_icon.png"
+          alt="Slika"
+          width={16}
+          height={20}
+          className="cursor-pointer"
+        />
+      </div>
+    );
+  }
+
   return (
-    <div className="w-[440px] h-[300px] relative mx-auto">
+    <div className="w-[440px] h-[300px] relative desktop:mx-auto">
       <Slider {...settings}>
         {images.map((cemetery, index) => (
           <div
